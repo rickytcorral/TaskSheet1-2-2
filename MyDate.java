567e0e511bc2b61a73ccbe86821707b7d7677c4c
rickytcorral <154580337+rickytcorral@users.noreply.github.com>
2024-02-08 12:53:21 +0000

    done

--- MyDate.java
+++ MyDate.java
@@ -1,1 +1,12 @@
 
+
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask();
+        DateTask date2 = new DateTask();
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+       // display leapyears
+       date2.leapYears();
+    }
+}
